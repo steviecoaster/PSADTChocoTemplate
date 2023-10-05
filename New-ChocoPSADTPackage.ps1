0fbec935767d82f40f2c38139381d871fcb317c1
Stephen Valdinger <stephen@chocolatey.io>
2023-10-04 21:16:53 -0400

    Add initial template files and controller script.

---
+++ New-ChocoPSADTPackage.ps1
@@ -0,0 +1,65 @@
+﻿[cmdletBinding()]
+Param(
+    [Parameter(Mandatory)]
+    [String]
+    $PackageName,
+
+    [Parameter(Mandatory)]
+    [ValidateScript({
+        Test-Path $_
+    })]
+    [String]
+    $PackageZip,
+
+    [Parameter(Mandatory)]
+    [String]
+    $SoftwareName,
+
+    [Parameter(Mandatory)]
+    [ValidateSet('PSADT-Silent','PSADT-Install')]
+    $Template,
+
+    [Parameter()]
+    [string]
+    $PackageVersion = '1.0.0',
+
+    [Parameter()]
+    [String]
+    $OutputDirectory = (Split-Path -Parent $MyInvocation.MyCommand.Definition),
+
+    [Parameter()]
+    [Switch]
+    $BuildPackage
+)
+
+process {
+    $PackageName = ($PackageName).ToLower()
+
+    if($OutputDirectory){
+        if(-not (Test-Path $OutputDirectory)){
+            $null = New-Item $OutputDirectory -ItemType Directory
+        }
+    }
+
+    $Zip = $PackageZip.Split('\')[-1]
+
+    Write-Verbose "Received zip: $Zip"
+
+    $chocoArgs = @('new',$PackageName,"--version='$PackageVersion'","--template='$Template'","--output-directory='$OutputDirectory'","Software=$SoftwareName","Zip=$Zip")
+
+    Write-Verbose -Message "Received following arguments for choco; $($chocoArgs -join ' ')"
+
+    
+    & choco @chocoArgs
+
+    $packageDirectory = Join-Path $OutputDirectory -ChildPath $PackageName
+    $toolsDirectory = Join-Path $packageDirectory -ChildPath 'tools'
+
+    Copy-Item $PackageZip -Destination $toolsDirectory
+    
+    if($BuildPackage){
+       Push-Location $packageDirectory
+       & choco pack
+       Pop-Location
+    }
+}
